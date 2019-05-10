77da8d6cb6e860b54e62621cac80f7295532bfd3
Haruka Iwanami <s1711432@s.tsukuba.ac.jp>
2019-05-10 17:18:13 +0900

    基本情報の表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Eatery.delete_all
-Eatery.create(name: "te.to.te",address:"茨城県つくば市桜2丁目1-7 グランパスつくば102号",tel:"029-857-7793",regular_holiday:"不定休",open:"11:30",close:"24:00")
-Eatery.create(name: "ソラカフェ",address:"茨城県つくば市天久保3-2-8ウッディライフつくば1F",tel:"029-886-9750",regular_holiday:"火曜日",open:"11:30",close:"21:00(LO 20:00)"
+Eatery.create(name:"te.to.te",address:"茨城県つくば市桜2丁目1-7 グランパスつくば102号",tel:"029-857-7793",regular_holiday:"不定休",open:"11:30",close:"24:00")
+Eatery.create(name:"ソラカフェ",address:"茨城県つくば市天久保3-2-8ウッディライフつくば1F",tel:"029-886-9750",regular_holiday:"火曜日",open:"11:30",close:"21:00(LO 20:00)"
 )
