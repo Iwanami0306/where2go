4cb7abd39cb0be712f993746c889cd76565acfea
Haruka Iwanami <s1711432@s.tsukuba.ac.jp>
2019-05-17 18:22:26 +0900

    ログインメッセージの表示

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
+# coding: utf-8
 class SessionsController < ApplicationController
   def new
+  end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    if user&.authentiicate(session_params[:password])
+      session[:user_id] = user.id
+      flash[:success] = 'ログインに成功しました'
+      redirect_to root_path
+    else
+      flash[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end
