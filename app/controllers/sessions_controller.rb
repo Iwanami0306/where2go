3c4bf4ad0a2d3840dce46f498e1c4d4b4424e5fc
Haruka Iwanami <s1711432@s.tsukuba.ac.jp>
2019-05-24 13:22:55 +0900

    ログインと登録機能の制限の追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: session_params[:email])
 
-    if user&.authentiicate(session_params[:password])
+    if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
       flash[:success] = 'ログインに成功しました'
       redirect_to root_path
