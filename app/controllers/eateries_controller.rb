b5bac4a9ef5269d8bc4fea3b6d696e510bbaca22
Haruka Iwanami <s1711432@s.tsukuba.ac.jp>
2019-05-24 11:33:27 +0900

    ログインと登録機能の制限の追加

--- app/controllers/eateries_controller.rb
+++ app/controllers/eateries_controller.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class EateriesController < ApplicationController
-before_action :login_required
+
   def new
   end
 
@@ -15,14 +15,6 @@ private
 
 def eatery_params
   params.require(:eatery).permit(:name)
-end
-
-def user_params
-  params.require(:user).permit(:name,:email,:password,:password_confirmation)
-end
-
-def login_required
-  redirect_to login_path unless current_user
 end
 
 end
